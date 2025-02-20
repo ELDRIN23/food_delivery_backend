d2c95a55e22f9901a0fe946695e44da091048c05
eldrin23 <eldrinjohnsom230@gmail.com>
2025-02-21 02:46:04 +0530

    updated

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,6 +5,7 @@ import { processUpload } from "../config/cloudinaryConfig.js";
 import { generatePaymentIntent } from "../controllers/payment.js";
 import { createOrder, fetchOrderDetails } from "../controllers/orderController.js";
 import imageUpload from "../middlewares/ImageUpload/imageUpload.js";
+import { getAllUsers } from "../controllers/userControllers.js";
 
 const router = express.Router();
 
@@ -27,29 +28,8 @@ router.post("/create-payment-intent" , userAthmiddleware , generatePaymentIntent
 
 router.get('/create-order' , userAthmiddleware , createOrder)
 router.get('/fetch-orders' , userAthmiddleware , fetchOrderDetails)
+router.get("/users", userAthmiddleware, getAllUsers);
+
 
 export {router as userRouter};
 
-// import express from "express";
-// import { userRegister, userSignin, userLogout, userProfile, updateUserProfile } from "../controllers/userControllers.js";
-// import { userAthmiddleware } from "../middlewares/userAuth.js"; 
-// // import { upload } from "../middlewares/multer.js";
-
-// const router = express.Router();
-
-// // Register
-// router.post('/signup',userRegister);
-
-// // Signin
-// router.post('/login', userSignin);
-
-// // Logout
-// router.get('/logout', userAthmiddleware, userLogout);
-
-// // Profile
-// router.get('/profile', userAthmiddleware, userProfile);
-
-// // Update Profile
-// router.put('/updateprofile', userAthmiddleware, updateUserProfile);
-
-// export { router as userRouter };
